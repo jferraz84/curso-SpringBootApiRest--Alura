f1d28ded700f74f4a75f8594a6109ecf8d1a17c6
Jorge Henrique Dos Santos Ferraz <jferraz@cpqd.com.br>
2021-04-09 11:21:31 -0300

    Spring Boot API Rest Mod3: Mais seguranca

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -45,9 +45,8 @@ public class TopicosController {
 
 	@GetMapping
 	@Cacheable(value = "listaDeTopicos")
-	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso, 
-			                     @PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10)Pageable paginacao) {
-		
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort = "id", direction = Direction.DESC, page = 0, size = 10) Pageable paginacao) {
 
 		if (nomeCurso == null) {
 			Page<Topico> topicos = topicoRepository.findAll(paginacao);
